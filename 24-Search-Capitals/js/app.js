bb961746b299e79f8e46d2baba8eea081cdf649c
Noa <noasalgadodev@gmail.com>
2023-01-02 11:00:58 +0100

    Add input validation and show error when no matches are found

--- 24-Search-Capitals/js/app.js
+++ 24-Search-Capitals/js/app.js
@@ -2,6 +2,7 @@ const countries = [];
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const countriesList = document.querySelector('ul');
+const notFoundText = document.querySelector('p');
 
 const getCountries = async () => {
   await fetch('https://restcountries.com/v2/all')
@@ -13,12 +14,25 @@ window.addEventListener('load', getCountries);
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  if (!input.value.trim()) {
+    return;
+  }
   userSearch = input.value.toLocaleLowerCase();
-  countries.forEach((country) => {
-    if (country.name.toLocaleLowerCase().includes(userSearch)) {
+  input.value = '';
+  countriesList.innerHTML = '';
+  notFoundText.classList.remove('display');
+  const matchingCountries = countries.filter((country) =>
+    country.name.toLocaleLowerCase().includes(userSearch)
+  );
+
+  if (matchingCountries.length) {
+    matchingCountries.forEach((country) => {
       const countryElement = document.createElement('li');
       countryElement.innerHTML = `<span>${country.name}</span>: ${country.capital}`;
       countriesList.append(countryElement);
-    }
-  });
+    });
+  } else {
+    notFoundText.classList.add('display');
+  }
 });
